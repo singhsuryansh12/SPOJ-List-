42206cfb32000ed31803073d35ba6a7c830f75ba
Suryansh Singh <Suryansh Singh>
2020-04-07 22:37:47 +0530

    CARDS Done

--- crds.c
+++ crds.c
@@ -2,15 +2,17 @@
 #include <math.h>
 
 int main(void){
-    long long int n, i, level[2000], cards[2000],c=1000007;
-    long double x;
+    unsigned long long int n, i, level[2000], cards[2000],c=1000007;
+    unsigned long long x;
     scanf("%lld",&n);
     for(i=0;i<n;i++){
         scanf("%lld",&level[i]);
     }
     for(i=0;i<n;i++){
         x=level[i];
-        cards[i]=fmod((fmod((x/2),c)*fmod((3*x + 1),c)),c);
+     //   cards[i]=fmod((fmod((x/2),c)*fmod((3*x + 1),c)),c);
+        cards[i]=0.5*(3*x*x + x);
+        cards[i] = cards[i]%c;
         printf("%lld\n",cards[i]);
     }
 }
